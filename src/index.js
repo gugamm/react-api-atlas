1f4e0c4a84ed204d98f94334e4c76f5844ffad7c
gugamm <gugamm@live.com>
2017-09-18 23:41:46 -0300

    export api-atlas modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import AtlasProvider from './atlas-provider';
 import atlas from './atlas';
+import { AtlasClient, AtlasMap, createNetworkInterface } from 'api-atlas';
 
 export {
   AtlasProvider,
   atlas,
+  AtlasClient,
+  AtlasMap,
+  createNetworkInterface
 };
